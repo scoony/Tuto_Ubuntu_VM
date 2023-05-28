ecc3fd0c4ce753ca54021de62b4b4795360c1b0b
GUY Jérôme <scoonydeus@gmail.com>
2023-05-28 14:07:51 +0200

    Create iommu_grp.sh

---
+++ scripts/iommu_grp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# change the 999 if needed
+shopt -s nullglob
+for d in /sys/kernel/iommu_groups/{0..999}/devices/*; do
+    n=${d#*/iommu_groups/*}; n=${n%%/*}
+    printf 'IOMMU Group %s ' "$n"
+    lspci -nns "${d##*/}"
+done;
