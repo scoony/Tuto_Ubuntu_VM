5ff2ad9b31a618a7d478be438752045ac8a39228
Scoony <scoonydeus@gmail.com>
2023-06-05 00:14:14 +0200

    Update

--- scripts/resizable_bar.sh
+++ scripts/resizable_bar.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Bit Sizes
+# 1 = 2MB
+# 2 = 4MB
+# 3 = 8MB
+# 4 = 16MB
+# 5 = 32MB
+# 6 = 64MB
+# 7 = 128MB
+# 8 = 256MB
+# 9 = 512MB
+# 10 = 1GB
+# 11 = 2GB
+# 12 = 4GB
+# 13 = 8GB
+# 14 = 16GB
+# 15 = 32GB
+
+## J'ai mis 32/8
+
 echo -n "0000:03:00.0" > /sys/bus/pci/drivers/vfio-pci/unbind
 echo "15" > /sys/bus/pci/devices/0000\:03\:00.0/resource0_resize
 echo "3" > /sys/bus/pci/devices/0000\:03\:00.0/resource2_resize
