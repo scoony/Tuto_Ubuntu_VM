c2561b61b031e6999661a9049d350344b978f098
Scoony <scoonydeus@gmail.com>
2023-06-05 00:13:05 +0200

    Update

--- scripts/resizable_bar.sh
+++ scripts/resizable_bar.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-
+echo -n "0000:03:00.0" > /sys/bus/pci/drivers/vfio-pci/unbind
+echo "15" > /sys/bus/pci/devices/0000\:03\:00.0/resource0_resize
+echo "3" > /sys/bus/pci/devices/0000\:03\:00.0/resource2_resize
+echo -n "0000:03:00.0" > /sys/bus/pci/drivers/vfio-pci/bind
